3c68851269ab1f62f530482a3cb304ac7f4010ee
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-11-14 16:18:57 +0100

    Update use-current-user.hook.tsx

--- hooks/use-current-user.hook.tsx
+++ hooks/use-current-user.hook.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -7,7 +8,7 @@ import { useEffect, useState } from 'react';
 
 export default function useCurrentUser() {
 	const [userAddr, setUserAddr] = useState(null);
-	const [chainId, setChainId] = useState(null);
+	const [chainId, setChainId] = useState<any>(null);
 	const [walletSdk, setWalletSdk] = useState<any>(null);
 	const DEFAULT_CHAIN_ID = '0x747'; // Flow EVM Mainnet: 747 Flow EVM Testnet: 545
 
@@ -30,18 +31,35 @@ export default function useCurrentUser() {
 		};
 	}, []);
 
+	function initEventListener() {
+		// events
+		walletSdk.ethereum.removeAllListeners();
+		function handleAccountsChanged(accounts: any) {
+			setUserAddr(accounts[0]);
+		}
+		function handleChainChanged(_chainId: any) {
+			setChainId('0x' + Number(_chainId).toString(16));
+		}
+
+		walletSdk.ethereum.on('accountsChanged', handleAccountsChanged);
+		walletSdk.ethereum.on('chainChanged', handleChainChanged);
+	}
+
 	const logIn = async () => {
 		try {
 			const accounts = await walletSdk.ethereum.request({
 				method: 'eth_requestAccounts',
 			});
-			setUserAddr(accounts[0]);
+
 			const chainId = await walletSdk.ethereum.request({
 				method: 'eth_chainId',
 				params: [],
 			});
+			const isConnected = accounts[0];
+			setUserAddr(accounts[0]);
 			setChainId(chainId);
-			switchChain(DEFAULT_CHAIN_ID);
+			initEventListener();
+			isConnected && switchChain(DEFAULT_CHAIN_ID);
 		} catch (error) {
 			console.error('Failed to connect wallet:', error);
 		}
